6c86303ccdb8d1e22327e184248d675da8716934
Neo Wang <wiirhan@163.com>
2024-08-04 22:47:22 +0800

    feat: change element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from 'next'
-import { H1 } from '../components/Typography'
 
 const Home: NextPage = () => {
   return (
@@ -7,10 +6,10 @@ const Home: NextPage = () => {
       <div className="container p-4 mx-auto flex justify-between">
         <div className="flex flex-col gap-y-6">
           <span className="leading-text text-text-dark-accent">MODERN STUDIO</span>
-          <div className="flex flex-col">
-            <H1 className="text-text-light">We’re Help</H1>
-            <H1 className="text-text-light">To Build Your</H1>
-            <H1 className="text-text-light">Dream Project</H1>
+          <div className="text-7xl font-extrabold tracking-tighter text-white leading-[80px] max-w-[513px] max-md:text-4xl max-md:leading-[49px]">
+            We’re Help <br />
+            To Build Your <br />
+            Dream Project
           </div>
           <div>
             <span className="text-text-light-gray leading-8">Agency provides a full service range including technical skills, design, business understanding.</span>
